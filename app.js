e377e153f8eed7b2c778e75ea576706d6a330336
Beatriz Diaz <isc18350304@gmail.com>
2021-11-24 23:43:37 -0600

    Estructura inicial de la API

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const app = express();
 
-app.get('/', function (req, res) {res.send('Hola Mundo');});
+require ('dotenv').config();
 
-app.listen(3000);
+const Server = require ('./Server')
+
+const server = new Server;
+
+server.listen();
