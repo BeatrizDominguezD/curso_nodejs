c843e26218225b7ff58fd597791cf78c12900776
Beatriz Diaz <isc18350304@gmail.com>
2022-01-12 19:28:32 -0600

    Modificaciones a los EndPoints

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,17 +1,72 @@
 const {request, response} = require ("express");
+const pool = require("../db/conexion");
+const usuariosQueries = require("../models/usuarios");
 
-const usuariosGet = (req = request, res = response) => {
-    res.json({ msg: "Hola pato desde GET"});
+const usuariosGet = async (req = request, res = response) => {
+    let conn;
+    try{
+        conn= await pool.getConnection();
+        const usuarios = await conn.query(usuariosQueries.selectUsuarios);
+
+        res.json({ usuarios });
+    } catch (error) {
+        console.log(error);
+        res
+         .status(500)
+         .json({ msg: "Plis contacta al admin uwu", error });
+    } finally {
+        if (conn) conn.end();
+    }
 };
 
-const usuariosPost = (req = request, res = response) => {
-    const {nombre, apellido, email, edad = 0} = req.body;
-    res.status(201).json({ msg: "Hola pato desde POST", edad});
+const usuariosPost = async (req = request, res = response) => {
+    const {nombre, email, password, status = 1} = req.body;
+
+    let conn;
+    try{
+        conn= await pool.getConnection();
+
+        const usuarios = await conn.query(usuariosQueries.insertUsuario, [
+            nombre, 
+            email, 
+            password, 
+            status
+        ]);
+
+        res.json({ usuarios });
+    } catch (error) {
+        console.log(error);
+        res
+         .status(500)
+         .json({ msg: "Plis contacta al admin uwu", error });
+    } finally {
+        if (conn) conn.end();
+    }
+   
 };
 
-const usuariosPut = (req = request, res = response) =>{
-    const { id } = req.params;
-    res.status(400).json({ msg: "Hola pato desde PUT",id});
+const usuariosPut = async (req = request, res = response) =>{
+    const { email } = req.query;
+    const { nombre, status } = req.body;
+
+    let conn;
+    try{
+        conn= await pool.getConnection();
+        const usuarios = await conn.query(usuariosQueries.updateUsuario, [
+            nombre,
+            status, 
+            email,
+        ]);
+
+        res.json({ usuarios });
+    } catch (error) {
+        console.log(error);
+        res
+         .status(500)
+         .json({ msg: "Plis contacta al admin uwu", error });
+    } finally {
+        if (conn) conn.end();
+    }
 };
 
 const usuariosDelete = (req = request, res = response) =>{
--- models/usuarios.js
+++ models/usuarios.js
@@ -2,21 +2,33 @@ const usuariosQueries ={
     insertUsuario: `
         INSERT INTO 
             usuarios(
-                nombre,
-                email,
-                password,
-                status
+              nombre,
+              email,
+              password,
+              status
             )
 
         VALUES
-            (?, ?, ?, ?)
+          (?, ?, ?, ?)
     `,
     selectUsuarios: `
-       SELECT
-         *
-       FROM
-         usuarios
-        WHERE
-          status = 1
-    `,        
+      SELECT
+        *
+      FROM
+        usuarios
+      WHERE
+        status = 1
+    `,
+  updateUsuario:`
+    UPDATE
+      usuarios
+    SET
+      nombre=?,
+      status=?
+    WHERE
+      email=?
+
+  `
 };
+
+module.exports = usuariosQueries;
